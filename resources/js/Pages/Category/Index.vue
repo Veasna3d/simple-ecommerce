cc693c7f36a72d2e11550dce3927d161540625df
Veasna <Veasna3d@gmail.com>
2022-11-11 15:56:23 +0700

    first commit

---
+++ resources/js/Pages/Category/Index.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <Head title="Category" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Category
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                       Table
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
